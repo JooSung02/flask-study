de2f6250ca9cebb8ae9ceac24d675110b847c387
Juseong Park <wntjdqkr156@gmail.com>
2025-07-16 14:18:04 +0900

    삭제 기능 추가

--- 04_Database/app.py
+++ 04_Database/app.py
@@ -19,6 +19,14 @@ def memo():  # 사용할 함수
     memos = Memo.query.all()  # DB에 저장된 모든 메모를 가져옴 , Memo 테이블에 저장된 모든 메모를 memo 리스트에 담아두기
     return render_template('memo.html', memos=memos) # memos라는 데이터를 html에 넘겨서 html 화면 출력
 
+@app.route('/delete/<int:memo_id>', methods=['POST'])  # 삭제 기능 (특정 memo의 id를 받아 삭제하는 방식) 이 경로로 POST가 오면 memo_id를 인자로 받아 삭제 처리
+def delete_memo(memo_id):  # 연결된 삭제 함수
+    memo_to_delete = Memo.query.get(memo_id)  # DB에서 해당 ID를 가진 메모를 가져옴  Memo.query.get은 기본 키를 기준으로 데이터를 가져오는 메서드
+    if memo_to_delete:  # 해당 id에 메모가 있으면 (= 삭제할 내용이 있으면)
+        db.session.delete(memo_to_delete)  # DB 세션에 이 메모를 삭제할 거라고 지정해둠
+        db.session.commit()  # 예약해둔 메모를 실제로 DB에서 삭제
+    return redirect(url_for('memo'))  # 다시 기본 화면으로 초기화 (memo() 함수와 연결된 라우트로 이동)
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
